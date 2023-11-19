46664d12c16ac708f82c1421c460d470cf14ed2b
Muxamedov Azamat <muxamedovazamat13@gmail.com git config --global user.email>
2023-11-19 20:00:59 +0500

    Initial commit

--- Draft.py
+++ Draft.py
@@ -1,2 +1,7 @@
+<<<<<<< HEAD
 
 
+=======
+list_1 = list(map(int, input("Vvedite cislo cherez probel: ").split(" ")))
+print(min(list_1), max(list_1))
+>>>>>>> fa3c854 (Initial commit)
---
+++ Урок 2. Циклы (for, while) (Задача №11).py
@@ -0,0 +1,18 @@
+# Задача №11. Решение в группах
+# Дано натуральное число A > 1. Определите, каким по
+# счету числом Фибоначчи оно является, то есть
+# выведите такое число n, что φ(n)=A. Если А не
+# является числом Фибоначчи, выведите число -1.
+# Input: 5
+# Output: 6
+
+a = int(input('Введите число Фибоначчи: '))
+fib_prev, fib_next = 0, 1
+count = 2
+while a > fib_next:
+    fib_prev, fib_next = fib_next, fib_prev + fib_next
+    count += 1
+if a == fib_next:
+    print(count)
+else:
+    print('-1')
