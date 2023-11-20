c66d52c43b60683dc932155933e77d4cb3f884b2
Muxamedov Azamat <muxamedovazamat13@gmail.com git config --global user.email>
2023-11-20 23:59:04 +0500

    Initial commit

--- Draft.py
+++ Draft.py
@@ -1,7 +1,5 @@
-<<<<<<< HEAD
-
-
-=======
-list_1 = list(map(int, input("Vvedite cislo cherez probel: ").split(" ")))
-print(min(list_1), max(list_1))
->>>>>>> fa3c854 (Initial commit)
+list_1 = list()
+for i in range(5):
+    n = int(input())
+    list_1.append(n)
+print(list_1)
